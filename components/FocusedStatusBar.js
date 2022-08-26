2363b0365f163315cab9723528a451aeb9d31e85
Pranit Puri <pranitpuri@Pran-MacBook-Air.local>
2022-08-26 06:12:30 +0530

    initial push

--- components/FocusedStatusBar.js
+++ components/FocusedStatusBar.js
@@ -1,4 +1,4 @@
-import { View, Text, StatusBar } from "react-native";
+import { View, Text, StatusBar, Platform } from "react-native";
 import React from "react";
 
 const FocusedStatusBar = ({ backgroundColor, ...props }) => {
