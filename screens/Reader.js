0113c5c958dcb710fae75bc435834e2e897a0681
Pranit Puri <pranitpuri@Pran-MacBook-Air.local>
2022-08-26 06:13:21 +0530

    initial push

--- screens/Reader.js
+++ screens/Reader.js
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const Reader = () => {
   return (
     <View>
+      <FocusedStatusBar
+        backgroundColor={COLORS.primary}
+        barStyle="light-content"
+      />
       <Text>Reader</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Reader
+export default Reader;
