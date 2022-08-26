69917bc4f831c70654954a80b78d5ac46a1feca9
Pranit Puri <pranitpuri@Pran-MacBook-Air.local>
2022-08-26 06:11:22 +0530

    initial push

--- screens/Home.js
+++ screens/Home.js
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 
 import { FocusedStatusBar } from "../components";
+import { COLORS } from "../constants";
 
 const Home = () => {
   return (
