57d8af456e16e2df93fbf010af84a2bf00c42ae1
Pranit Puri <pranitpuri@Pran-MacBook-Air.local>
2022-08-26 06:10:47 +0530

    initial push

--- screens/Admin.js
+++ screens/Admin.js
@@ -6,54 +6,57 @@ import { FocusedStatusBar, ImagePickerCard } from "../components";
 import { assets } from "../constants";
 
 const Admin = () => {
+  const displaySize = 280;
   const [pickedImage1, setPickedImage1] = useState(null);
   const [ocrImage1, setOcrImage1] = useState(null);
-  const [loading1, setLoading1] = useState(true);
+  // const [loading1, setLoading1] = useState(true);
   const [pickedImage2, setPickedImage2] = useState(null);
   const [ocrImage2, setOcrImage2] = useState(null);
-  const [loading2, setLoading2] = useState(true);
+  // const [loading2, setLoading2] = useState(true);
   const [similarity, setSimilarity] = useState(true);
 
   useEffect(() => {
     if (pickedImage1) {
+      let ocrImage = "";
       switch (pickedImage1.width) {
         case 1014:
-          setOcrImage1("https://i.postimg.cc/0y76bsDF/01-ocr.jpg");
+          ocrImage = "https://i.postimg.cc/0y76bsDF/01-ocr.jpg";
           break;
         case 1018:
-          setOcrImage1("https://i.postimg.cc/7LVfnnBN/02-ocr.jpg");
+          ocrImage = "https://i.postimg.cc/7LVfnnBN/02-ocr.jpg";
           break;
         case 910:
-          setOcrImage1("https://i.postimg.cc/9Mg0vFHR/03-ocr.jpg");
+          ocrImage = "https://i.postimg.cc/9Mg0vFHR/03-ocr.jpg";
           break;
         case 672:
-          setOcrImage1("https://i.postimg.cc/tJ1JyQ9r/04-ocr.jpg");
+          ocrImage = "https://i.postimg.cc/tJ1JyQ9r/04-ocr.jpg";
           break;
       }
       setTimeout(() => {
-        setLoading1(false);
+        setOcrImage1(ocrImage);
       }, Math.random() * 1500 + 580);
     }
   }, [pickedImage1]);
 
   useEffect(() => {
     if (pickedImage2) {
+      let ocrImage = "";
       switch (pickedImage2.width) {
         case 1014:
-          setOcrImage2("https://i.postimg.cc/0y76bsDF/01-ocr.jpg");
+          ocrImage = "https://i.postimg.cc/0y76bsDF/01-ocr.jpg";
           break;
         case 1018:
-          setOcrImage2("https://i.postimg.cc/7LVfnnBN/02-ocr.jpg");
+          ocrImage = "https://i.postimg.cc/7LVfnnBN/02-ocr.jpg";
           break;
         case 910:
-          setOcrImage2("https://i.postimg.cc/9Mg0vFHR/03-ocr.jpg");
+          ocrImage = "https://i.postimg.cc/9Mg0vFHR/03-ocr.jpg";
           break;
         case 672:
-          setOcrImage2("https://i.postimg.cc/tJ1JyQ9r/04-ocr.jpg");
+          ocrImage = "https://i.postimg.cc/tJ1JyQ9r/04-ocr.jpg";
           break;
       }
       setTimeout(() => {
-        setLoading2(false);
+        setOcrImage2(ocrImage);
       }, Math.random() * 1500 + 580);
     }
   }, [pickedImage2]);
@@ -70,7 +73,7 @@ const Admin = () => {
     <View
       style={{
         flexDirection: "column",
-        justifyContent: "space-evenly",
+        justifyContent: "space-between",
         alignItems: "center",
       }}
     >
@@ -83,23 +86,23 @@ const Admin = () => {
       >
         <ImagePickerCard
           text={"Pick First Image"}
-          displaySize={320}
+          displaySize={displaySize}
           pickedImage={pickedImage1}
           pickedImageFn={setPickedImage1}
         />
-        {ocrImage1 && !loading1 && (
+        {ocrImage1 && (
           <Image
             source={ocrImage1}
             resizeMode="cover"
             style={{
-              width: 320,
-              height: 320,
+              width: displaySize,
+              height: displaySize,
               opacity: 0.9,
             }}
           />
         )}
       </View>
-      {ocrImage1 && ocrImage2 && !loading1 && !loading2 && similarity && (
+      {ocrImage1 && ocrImage2 && similarity && (
         <Text>Similarity: {similarity}</Text>
       )}
       <View
@@ -111,17 +114,17 @@ const Admin = () => {
       >
         <ImagePickerCard
           text={"Pick Second Image"}
-          displaySize={320}
+          displaySize={displaySize}
           pickedImage={pickedImage2}
           pickedImageFn={setPickedImage2}
         />
-        {ocrImage2 && !loading2 && (
+        {ocrImage2 && (
           <Image
             source={ocrImage2}
             resizeMode="cover"
             style={{
-              width: 320,
-              height: 320,
+              width: displaySize,
+              height: displaySize,
               opacity: 0.9,
             }}
           />
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+
+import { FocusedStatusBar } from "../components";
 
 const Home = () => {
   return (
     <View>
+      <FocusedStatusBar
+        backgroundColor={COLORS.primary}
+        barStyle="light-content"
+      />
+
       <Text>Home</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
